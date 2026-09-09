734a295466206dd06a89bc50b859f7c3227b1470
marcos <msrserpa@gmail.com>
2026-09-09 09:24:25 -0300

    cuida.

--- src/MetodosEstaticos.java
+++ src/MetodosEstaticos.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Optional;
 
 public class MetodosEstaticos {
+
+    public static Optional<Integer> retornarUmaIdade(){
+        return Optional.ofNullable(null);
+    }
+
+    public static Integer retornarIdadePadrao(){
+        return 18;
+    }
 }
